d8ad3684049e91d21abecf39025f88566ffc2486
Zedrick Montante <montantezedrick2@gmail.com>
2025-09-14 22:52:02 +0800

    ako

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -3,7 +3,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class Welcome extends Controller {
 	public function index() {
-		$this->call->view('users/create');
+		$this->call->view('create');
 	}
 }
 ?>
